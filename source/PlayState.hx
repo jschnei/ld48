77e867a588882c8b0e7de59ad8880f4ad1ea9419
jon <noemail>
2021-04-23 20:50:42 -0400

    Remove export, add Player

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,8 +4,13 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	private var _player:Player;
+
 	override public function create()
 	{
+		_player = new Player(100, 100);
+		add(_player);
+
 		super.create();
 	}
 
