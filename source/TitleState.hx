81eae89d38950791b2a4452f9bebf73fa5a25812
Phillip Ai <pzai.577@gmail.com>
2021-04-25 19:38:23 -0700

    Added tiles to instructions

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -21,21 +21,22 @@ class TitleState extends FlxState
 
 		addBackground();
 
-		_titleText = new FlxText(0, 220, Registry.WINDOW_WIDTH, TITLE);
+		_titleText = new FlxText(0, 200, Registry.WINDOW_WIDTH, TITLE);
 		_titleText.setFormat(Registry.fontSource, 48, FlxColor.WHITE, FlxTextAlign.CENTER);
 		add(_titleText);
 
-		var insString = "The objective is to mine for treasure!\n
-		Drag your mouse along three tiles with the first and last matching.\n
-		The middle tile will drop to the next board, and the first and last tile will change color.\n
-		Use UP/DOWN or W/S to move between boards.\n
-		As you go deeper, there's better treasure to be found and more points to be gained!
-		";
-		_insText = new FlxText(0, 300, Registry.WINDOW_WIDTH, insString);
+		var insString = "The objective is to mine for treasure!
+		Drag your mouse along three tiles with the first and last matching.\n\n
+		The middle tile will drop to the next board, and the first and last tile will change color.
+		Use UP/DOWN or W/S to move between boards.
+		As you go deeper, there's better treasure to be found and more points to be gained!";
+		_insText = new FlxText(0, 280, Registry.WINDOW_WIDTH, insString);
 		_insText.setFormat(Registry.fontSource, 16, FlxColor.WHITE, FlxTextAlign.CENTER);
 		add(_insText);
 
-		_startText = new FlxText(0, 680, Registry.WINDOW_WIDTH, "Start game");
+		addTileImages();
+
+		_startText = new FlxText(0, 670, Registry.WINDOW_WIDTH, "Start game");
 		_startText.setFormat(Registry.fontSource, 32, FlxColor.WHITE, FlxTextAlign.CENTER);
 		add(_startText);
 
@@ -65,5 +66,36 @@ class TitleState extends FlxState
 		_background.animation.add(flicker, [0, 1], 1, true);
 		_background.animation.play(flicker);
 		add(_background);
+	}
+
+	private function addTileImages():Void
+	{
+		var y = 370;
+
+		loadTileGraphics(
+			new FlxSprite((Registry.WINDOW_WIDTH - Grid.CELL_WIDTH) / 2, y),
+			new FlxSprite(x + Grid.CELL_WIDTH, y),
+			new FlxSprite(x + 2 * Grid.CELL_WIDTH, y),
+			AssetPaths.bluea__png,
+			AssetPaths.purplea__png);
+
+		colorA = AssetPaths.reda__png;
+		colorB = AssetPaths.yellowa__png;
+
+	}
+
+	private function loadTileGraphics(
+		sprite1:FlxSprite,
+		sprite2:FlxSprite,
+		sprite3:FlxSprite,
+		colorA:FlxGraphicAsset,
+		colorB:FlxGraphicAsset):Void
+	{
+		sprite1.loadGraphic(colorA, false, Grid.CELL_WIDTH, Grid.CELL_HEIGHT);
+		add(sprite1);
+		sprite2.loadGraphic(colorB, false, Grid.CELL_WIDTH, Grid.CELL_HEIGHT);
+		add(sprite2);
+		sprite3.loadGraphic(colorA, false, Grid.CELL_WIDTH, Grid.CELL_HEIGHT);
+		add(sprite3);
 	}
 }
